b50aa8e4ba2b873c33426b33c3802278eeccf1b2
Alexis Moreno <a.swerve97@gmail.com>
2022-01-06 12:32:58 -0500

    fixed post request issue and added a heroku file

--- app.py
+++ app.py
@@ -8,23 +8,13 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
-
-@app.route('/sub', methods=['POST'])
-def submit():
-    if request.method == "POST":
-        name = request.form['username']
-    return render_template('sub.html', n=name)
-
 @app.route('/date', methods=['POST'])
 def submit_date():
     if request.method == "POST":
-        year = request.form['year_request']
-    forecast_dict = predict('1980', year )
+        start_year = request.form['start_year']
+        end_year = request.form['end_year']
+    forecast_dict = predict(start_year, end_year )
     return  jsonify(forecast_dict) 
-
-@app.route('/myForm', methods=['POST'])
-def type_model():
-    pass
 
 
 if __name__ == '__main__':
