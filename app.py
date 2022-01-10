7ba910f8b7ff3ffda5917d260b6e2874c502eef9
Alexis Moreno <a.swerve97@gmail.com>
2022-01-09 19:26:22 -0500

    Added ability to use prophet model

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask.helpers import send_from_directory
 from arima import arima_predict
+from FB_prophet import prophet_predict
 
 app = Flask(__name__)
 
@@ -15,9 +16,12 @@ def submit_date():
         start_year = request.form['start_year']
         end_year = request.form['end_year']
     
-    forecast_dict = arima_predict(start_year, end_year )
-    return  jsonify(forecast_dict) 
-
+    if request.form['model'] == 'sarima':
+        forecast_dict = arima_predict(start_year, end_year )
+        return  jsonify(forecast_dict) 
+    elif request.form['model'] == 'prophet':
+        forecast_dict = arima_predict(start_year, end_year )
+        return  jsonify(forecast_dict) 
 @app.route('/actualPop', methods=['GET'])
 def return_actual_population():
     return send_from_directory('static', 'actualUSPopulation.json')
