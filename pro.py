bc4c47730f89100f74d504e99f52eee8a391ea5c
Alexis Moreno <a.swerve97@gmail.com>
2022-01-20 17:42:42 -0500

    fixed the issues, everything seems to work

--- pro.py
+++ pro.py
@@ -6,7 +6,6 @@ con = psycopg2.connect(DATABASE_URL)
 start_of_trainng = "1900"
 ### end would be the year the FORECASTS STARTS
 end_of_training = "1910"
-#training_years = int(start_of_trainng) - int(end_of_training)
 projections_end = '1940'
 year_range = int(projections_end) - int(end_of_training)
 
@@ -31,6 +30,12 @@ m.fit(df)
 
 future = m.make_future_dataframe(periods = year_range, freq='YS')
 forecast = m.predict(future)
-a = forecast[['ds', 'yhat']].tail()
-#forecast = m.predict(future)
-print(a)
+
+forecast['yhat'] = forecast['yhat'].astype(int)
+
+forecast_dict = dict()
+for index, dates in enumerate(forecast['ds']):
+	forecast_dict[(dates.year)] = str(forecast['yhat'][index])
+	#if dates.year >= start and dates.year <= end and dates.month == 1 and dates.day == 1:
+	    
+print(forecast_dict)
