6126fd61d2451495a9a44e93ad04d5a822bdd612
Alexis Moreno <a.swerve97@gmail.com>
2022-01-20 03:00:28 -0500

    Moved files around, renamed files, and made a fresh and minimal copy of requirements.txt

--- test.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-import pandas as pd
-from secret import DATABASE_URL
-con = psycopg2.connect(DATABASE_URL)
-
-data = pd.read_sql('SELECT * FROM us_population', con = con)
-
-print(data)
-
