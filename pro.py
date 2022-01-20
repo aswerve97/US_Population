16367b3212fdddf8f65ffca2beaebf6601be1e52
Alexis Moreno <a.swerve97@gmail.com>
2022-01-20 16:19:36 -0500

    added some work

--- pro.py
+++ pro.py
@@ -5,9 +5,9 @@ from secret import DATABASE_URL
 con = psycopg2.connect(DATABASE_URL)
 start_of_trainng = "1900"
 ### end would be the year the FORECASTS STARTS
-end_of_training = "1910"
+end_of_training = "1920"
 #training_years = int(start_of_trainng) - int(end_of_training)
-projections_end = '1940'
+projections_end = '1975'
 year_range = int(projections_end) - int(end_of_training)
 
 df = pd.read_sql(
@@ -24,9 +24,21 @@ WHERE
 DS >= {start_of_trainng} AND DS <= {end_of_training}'''
     ,con = con)
 
-m = prophet.Prophet()
+convert_dict = {'ds': int,
+                'y': int }    
+df = df.astype(convert_dict)  
+
+m = prophet.Prophet(daily_seasonality=False, weekly_seasonality=False)
 m.fit(df)
 
-future = m.make_future_dataframe(periods = 5, freq='YS')
-forecast = m.predict(future)
+future = m.make_future_dataframe(periods = 30, freq='YS')
+
+print(future)
+
+#forecast = m.predict(future)
+
+#print(forecast)
+
+
+
 
